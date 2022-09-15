69cae813881a8c7b0f2561c1d36c9a48a271c476
Jordan Gangloff <jordangangloff@icloud.com>
2022-09-15 12:13:08 +0200

    generation of models

---
+++ db/migrate/20220915101206_create_missions.rb
@@ -0,0 +1,10 @@
+class CreateMissions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :missions do |t|
+      t.string :type_of_mission
+      t.references :listing, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
