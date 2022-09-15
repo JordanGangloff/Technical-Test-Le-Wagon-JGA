1af9d1133762410ab9d2a634bb7bb265cf2e0bd9
Jordan Gangloff <jordangangloff@icloud.com>
2022-09-15 15:41:42 +0200

    pause

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,24 +6,25 @@ class Booking < ApplicationRecord
 
   private
 
-  def create_first_check_in_mission
-    mission = Mission.new(listing_id: self.listing_id,
-                          type_of_mission: 'first_check_in',
-                          date: self.start_date,
-                          price: self.listing.num_rooms * 10)
-    mission.save
+  def create_first_checkin_mission
+    # Créer simultanément une mission avec type_of_mission first_checkin
+    first_checkin_mission = Mission.new(
+      listing_id: self.listing_id,
+      type_of_mission: 'first_checkin',
+      date: self.start_date,
+      price: self.listing.num_rooms * 10
+    )
+    first_checkin_mission.save
   end
 
   def create_last_checkout_mission
-    last_checkout_mission = Mission.where(type_of_mission: 'last_check_out',
-                                          date: self.end_date,
-                                          listing_id: self.listing_id)
-    if last_checkout_mission
-    else
-      mission = Mission.new(listing_id: self.listing_id, type_of_mission: 'first_check_in',
-                            date: self.end_date,
-                            price: self.listing.num_rooms * 10)
-      mission.save
-    end
+    # Créer simultanément une mission avec type_of_mission last_checkout
+    last_checkout_mission = Mission.new(
+      listing_id: self.listing_id,
+      type_of_mission: 'last_checkout',
+      date: self.end_date,
+      price: self.listing.num_rooms * 5
+    )
+    last_checkout_mission.save
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :listing
+
+  def possible?
+    Reservation.start_date <= Booking.start_date && Reservation.end_date <= Booking.end_date
+  end
 end
